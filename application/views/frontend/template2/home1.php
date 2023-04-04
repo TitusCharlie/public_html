e2ec1c0d965e988e5f509cc15e531fa2a8c4f584
TitusCharlie <freelancecharliet@gmail.com>
2023-04-04 12:18:22 +0100

    adjusted application/controllers/Auth.py from line 179 to 214

---
+++ application/views/frontend/template2/home1.php
@@ -0,0 +1,366 @@
+
+<!DOCTYPE html>
+<html lang="en"><head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Best Cryptocurrency Wallet | Ethereum Wallet | ERC20 Wallet | Trust Wallet</title>
+<meta property="og:title" content="Best Cryptocurrency Wallet | Ethereum Wallet | ERC20 Wallet | Trust Wallet" />
+<meta name="description" content="Trust Wallet is the best ethereum wallet and cryptocurrency wallet to store your favourite BEP2, ERC20 and ERC721, tokens. Download the Android Trust Wallet and iOS app today!" />
+<meta property="og:description" content="Trust Wallet is the best ethereum wallet and cryptocurrency wallet to store your favourite BEP2, ERC20 and ERC721, tokens. Download the Android Trust Wallet and iOS app today!" />
+<meta name="twitter:card" content="summary_large_image" />
+<meta property="twitter:image" content="https://trustwallet.com/assets/images/media/preview/horizontal_blue.png" />
+<meta name="twitter:creator" content="@trustwalletapp" />
+<meta property="og:locale" content="en" />
+<meta property="og:image" content="https://trustwallet.com/assets/images/media/preview/horizontal_blue.png" />
+<meta property="og:type" content="website" />
+<link rel="canonical" href="https://trustwallet.com/" />
+<meta property="og:url" content="https://trustwallet.com/" />
+<meta property="og:site_name" content="Trust Wallet" />
+<meta property="al:ios:app_name" content="Trust Wallet" />
+<meta property="al:ios:app_store_id" content="1288339409" />
+<meta property="al:ios:url" content="trust://" />
+<meta property="al:android:app_name" content="Trust Wallet" />
+<meta property="al:android:package" content="com.wallet.crypto.trustapp" />
+<meta property="al:android:url" content="https://trustwallet.com" />
+<link rel="shortcut icon" href="/assets/images/favicon.ico" type="image/x-icon">
+<link rel="shortcut icon" type="image/png" href="/assets/images/favicon.png" />
+<meta name="apple-itunes-app" content="app-id=1288339409,affiliate-data=11l7ss">
+<meta name="theme-color" content="#3375BB">
+<script type="application/ld+json">
+    {
+      "description": "Trust Wallet is the best ethereum wallet and cryptocurrency wallet to store your favourite BEP2, ERC20 and ERC721, tokens. Download the Android Trust Wallet and iOS app today!",
+      "@type": "WebPage",
+      "url": "https://trustwallet.com/",
+      "headline": "Best Cryptocurrency Wallet | Ethereum Wallet | ERC20 Wallet | Trust Wallet",
+      "@context": "https://schema.org"
+    }
+  </script><script async defer data-domain="trustwallet.com" src="https://analytics.trustwallet.com/js/plausible.js"></script>
+<script>window.plausible = window.plausible || function() { (window.plausible.q = window.plausible.q || []).push(arguments) }</script>
+<script type="application/javascript">
+    document.addEventListener("DOMContentLoaded", function(event) {
+      document.getElementById('navbarToggle').addEventListener('click', function() {
+        var el = document.getElementById('navbarSupportedContent');
+        var toggled = window.getComputedStyle(el).display == 'block';
+        if (toggled) {
+          el.style.display = "none";
+        } else {
+          el.style.display = "block";
+        }
+      })
+      //Add Huawei download button based on device.
+      var result = new UAParser().getResult();
+      var apple = document.getElementsByClassName("apple");
+      var huawei = document.getElementsByClassName("huawei");
+      var download = document.getElementsByClassName("downloadapp-native");
+
+      if (result.os.name == "Android") {
+        for (var i = 0; i < huawei.length; i++) {
+          huawei[i].classList.remove("hidden");
+          apple[i].classList.add("hidden");
+        }
+        // if it is android change ctas to googleplay
+        for (var i = 0; i < download.length; i++) {
+          download[i].href = "https://play.google.com/store/apps/details?id=com.wallet.crypto.trustapp&referrer=utm_source%3Dwebsite";
+        }
+      }
+      // if its apple change ctas to appstore
+       else if (result.os.name == "iOS") {
+        for (var i = 0; i < download.length; i++) {
+          download[i].href = "https://apps.apple.com/app/apple-store/id1288339409?mt=8";
+        }
+      }
+    });
+  </script>
+<noscript>Your browser does not support JavaScript!</noscript>
+<link rel="preload" href="/assets/fonts/IBMPlexSans/IBMPlexSans-Regular.woff2" as="font" type="font/woff2" crossorigin>
+<link rel="preload" href="/assets/fonts/IBMPlexSans/IBMPlexSans-Bold.woff2" as="font" type="font/woff2" crossorigin>
+<link rel="preload" href="/assets/fonts/IBMPlexSans/IBMPlexSans-Medium.woff2" as="font" type="font/woff2" crossorigin>
+<link rel="stylesheet" href="/css/main.css" />
+<link rel="alternate" href="https://trustwallet.com/" hreflang="en" />
+<link rel="alternate" href="https://trustwallet.com/" hreflang="x-default" /><link rel="alternate" href="https://trustwallet.com/ru/" hreflang="ru" /><link rel="alternate" href="https://trustwallet.com/de/" hreflang="de" /><link rel="alternate" href="https://trustwallet.com/vi/" hreflang="vi" /><link rel="alternate" href="https://trustwallet.com/id/" hreflang="id" /><link rel="alternate" href="https://trustwallet.com/ko/" hreflang="ko" /><link rel="alternate" href="https://trustwallet.com/ja/" hreflang="ja" /><link rel="alternate" href="https://trustwallet.com/tr/" hreflang="tr" /><link rel="alternate" href="https://trustwallet.com/pt_BR/" hreflang="pt" /><link rel="alternate" href="https://trustwallet.com/zh_CN/" hreflang="zh" /><link rel="alternate" href="https://trustwallet.com/es/" hreflang="es" /><script type="text/javascript" src="../assets/js/platform.js"></script>
+<script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+<base href="https://trustwallet.com">
+</head>
+<body> 
+<main class="page-content" aria-label="Content">
+<section class="pt-8 pb-8 bg-gradient-primary">
+<div class="container">
+<div class="row align-items-center justify-content-center justify-content-md-between">
+<div class="col-12 col-lg-6 mb-8 text-center text-md-left text-white">
+<h1 class="display-3 font-weight-bold">The most trusted &amp; secure crypto wallet</h1>
+<p class="text-white-90 lead mb-4">Buy, store, collect NFTs, exchange &amp; earn crypto. Join 25 million+ people using Trust Wallet.</p><div class="download-button">
+<div class="row download">
+<div class="col-12 col-lg-4 apple">
+<a href="https://apps.apple.com/app/apple-store/id1288339409?mt=8" data-analytics="&quot;Download Clicks&quot;, {&quot;props&quot;:{&quot;Target&quot;:&quot;App Store&quot;}}" class="downloadapp" aria-label="goes to AppStore to download Trust Wallet for iphones">
+<svg class="download-app-image download-apple" loading="lazy" role="img" alt="goes to AppStore to download Trust Wallet for iphones" aria-label="goes to AppStore to download Trust Wallet for iphones" rel="noopener"></svg>
+</a>
+</div>
+<div class="col-12 col-lg-4 googleplay">
+<a href="https://play.google.com/store/apps/details?id=com.wallet.crypto.trustapp&amp;referrer=utm_source%3Dwebsite" data-analytics="&quot;Download Clicks&quot;, {&quot;props&quot;:{&quot;Target&quot;:&quot;Google Play&quot;}}" class="downloadapp" aria-label="goes to Google Play store to download Trust Wallet for Android phones">
+<svg class="download-app-image download-play" loading="lazy" role="img" alt="goes to Google Play store to download Trust Wallet for Android phones" aria-label="goes to Google Play store to download Trust Wallet for Android phones" rel="noopener"></svg>
+</a>
+</div>
+<div class="col-12 col-lg-4 androidapk">
+<a href="https://trustwallet.com/dl/apk" data-analytics="&quot;Download Clicks&quot;, {&quot;props&quot;:{&quot;Target&quot;:&quot;Download Apk&quot;}}" class="downloadapp" aria-label="Downloads Trust Wallet as APK file for android phones">
+<svg class="download-app-image download-apk" loading="lazy" role="img" alt="Downloads Trust Wallet as APK file for android phones" aria-label="Downloads Trust Wallet as APK file for android phones" rel="noopener"></svg>
+</a>
+</div>
+<div class="col-12 col-lg-4 huawei hidden">
+<a href="https://appgallery.huawei.com/#/app/C103425801" data-analytics="&quot;Download Clicks&quot;, {&quot;props&quot;:{&quot;Target&quot;:&quot;Huawei AppGallery&quot;}}" class="downloadapp" aria-label="goes to huawei store to download Trust Wallet for huawei phones">
+<svg class="download-app-image download-huawei" loading="lazy" role="img" alt="goes to huawei store to download Trust Wallet for huawei phones" aria-label="goes to huawei store to download Trust Wallet for huawei phones" rel="noopener"></svg>
+</a>
+</div>
+<div class="col-12 col-lg-4">
+<a href="https://chrome.google.com/webstore/detail/trust-wallet/egjidjbpglichdcondbcbdnbeeppgdph" data-analytics="&quot;Download Clicks&quot;, {&quot;props&quot;:{&quot;Target&quot;:&quot;Google Chrome extension&quot;}}" class="downloadapp" aria-label="goes to Google Chrome web store">
+<svg class="download-app-image download-chrome" loading="lazy" role="img" alt="goes to Google Chrome webs tore" aria-label="goes to Google Chrome web store" rel="noopener"></svg>
+</a>
+</div>
+</div>
+</div></div>
+<div class="col-12 col-lg-6 text-center">
+<img loading="lazy" class="hero-image mx-auto d-block img-fluid" src="/assets/images/home_hero.png" alt="Trust Wallet app mobile mockup">
+</div>
+</div>
+</div>
+</section>
+
+<section class="pt-8 pb-8 bg-gradient-primary">
+<div class="container">
+<div class="row align-items-center justify-content-center justify-content-md-between">
+<div class="col-12 col-lg-4">
+<a href="https://chrome.google.com/webstore/detail/trust-wallet/egjidjbpglichdcondbcbdnbeeppgdph" data-analytics='"Download Clicks", {"props":{"Target":"Google Chrome extension"}}' class="downloadapp" aria-label="goes to Google Chrome web store">
+<svg class='download-app-image download-chrome' loading='lazy' role="img" alt="goes to Google Chrome webs tore" aria-label="goes to Google Chrome web store" rel="noopener"></svg>
+</a>
+</div>
+</div>
+</div></div>
+
+</div>
+</div>
+</section>
+<section class="pt-6">
+<div class="container">
+<div class="row border-bottom justify-content-center pb-4">
+<div class="col-12 col-md-auto mr-auto d-flex justify-content-center">
+<div class="icon icon-xs mb-5">
+<svg alt="icon card" title="Credit card icon">
+<use xlink:href="/assets/images/features-2.svg#card"></use>
+</svg>
+</div>
+<div class="ml-4">
+<span class="font-weight-bold lead">
+Buy Crypto With a Card
+</span>
+</div>
+</div>
+<div class="col-12 col-md-auto d-flex justify-content-center">
+<div class="icon icon-xs mb-5">
+<svg alt="icon dex" title="Arrow shape icon">
+<use xlink:href="/assets/images/features-2.svg#dex"></use>
+</svg>
+</div>
+<div class="ml-4">
+<span class="font-weight-bold lead">
+Exchange Instantly
+</span>
+</div>
+</div>
+<div class="col-12 col-md-auto ml-auto d-flex justify-content-center">
+<div class="icon icon-xs mb-5">
+<svg alt="icon lock" title="Key lock icon">
+<use xlink:href="/assets/images/features-2.svg#lock"></use>
+</svg>
+</div>
+<div class="ml-4">
+<span class="font-weight-bold lead">
+Private & Secure
+</span>
+</div>
+</div>
+</div>
+</div>
+</section>
+<section class="pt-8">
+<div class="container">
+<div class="row align-items-center justify-content-center justify-content-md-between">
+<div class="col-12 text-center mt-4 mt-md-0 order-md-2">
+<h2 class="display-4 font-weight-bold mb-4">You Deserve Easy Access to Cryptocurrencies</h2>
+<p class="lead text-gray-700">Trust Wallet is for you if you want to</p>
+</div>
+</div>
+<div class="row align-items-center justify-content-center justify-content-md-between pt-4 pb-4 text-center">
+<div class="col-12 col-lg-6">
+<div class="list-group-item d-flex"><span class="mr-4"><em class="check"></em></span>Buy Bitcoin in under five minutes</div>
+<div class="list-group-item d-flex"><span class="mr-4"><em class="check"></em></span>Easily earn interest on the crypto in your wallet</div>
+<div class="list-group-item d-flex"><span class="mr-4"><em class="check"></em></span>See your collectibles. Art & NFTs in one place</div>
+</div>
+<div class="col-12 col-lg-6">
+<div class="list-group-item d-flex"><span class="mr-4"><em class="check"></em></span>Exchange your crypto without leaving the app</div>
+<div class="list-group-item d-flex"><span class="mr-4"><em class="check"></em></span>Track charts and prices within the app</div>
+<div class="list-group-item d-flex"><span class="mr-4"><em class="check"></em></span>Keep your crypto safe from hackers & scammers</div>
+</div>
+<a href="/download" class="btn btn-primary mt-6 mx-auto downloadapp-native" aria-label="goes to download page to get Trust Wallet App">
+Download Now
+</a>
+</div>
+</div>
+</section>
+<section class="pt-8">
+<div class="container">
+<div class="row align-items-center justify-content-center justify-content-md-between">
+<div class="col-12 col-md-6 col-lg-5 text-center text-md-left mb-4 order-sm-first order-md-2">
+<h2 class="display-4 font-weight-bold mt-4">Buy Crypto With a Card</h2>
+<p class="lead text-gray-700">Get your first $50 of Bitcoin, Ethereum, Binance Coin and many other cryptocurrencies.</p>
+</div>
+<div class="col-12 col-md-6 col-lg-7">
+<img loading="lazy" class="img-fluid mx-auto d-block" src="/assets/images/home_cards.png" alt="Trust Wallet mobile mockup shows how it looks like to buy crypto with credit card">
+</div>
+</div>
+<div class="row align-items-center justify-content-center justify-content-md-between">
+<div class="col-12 col-md-6 col-lg-5 text-center text-md-left mb-4">
+<h2 class="display-4 font-weight-bold mt-8">Exchange Instantly</h2>
+<p class="lead text-gray-700">No forms, no selfies. Trade crypto anytime with ease.</p>
+</div>
+<div class="col-12 col-md-6 col-lg-7">
+<img loading="lazy" class="img-fluid  mx-auto d-block" src="/assets/images/home_dex.png" alt="Trust Wallet mobile mockup shows an example how swapping cryptocurrencies looks like">
+</div>
+</div>
+<div class="row align-items-center justify-content-center justify-content-md-between">
+<div class="col-12 col-md-6 col-lg-5 order-sm-2 text-center text-md-left mb-4 order-md-2">
+<h2 class="display-4 font-weight-bold mt-8">Private & Secure</h2>
+<p class="lead text-gray-700">Only you can access your wallet. We don’t collect any personal data.</p>
+</div>
+<div class="col-12 col-md-6 col-lg-7">
+<img loading="lazy" class="img-fluid mx-auto d-block" src="/assets/images/home_security.png" alt="Trust Wallet mobile mockup shows that wallet is private and secure">
+</div>
+</div>
+<div class="row align-items-center justify-content-center justify-content-md-between">
+<div class="col-12 col-md-6 col-lg-5 text-center text-md-left">
+<h2 class="display-4 font-weight-bold mt-8">Browser for DApps</h2>
+<p class="lead text-gray-700">Use your favourite decentralized apps & find new ones, without leaving your wallet.</p>
+</div>
+<div class="col-12 col-md-6 col-lg-7">
+<img loading="lazy" class="img-fluid mx-auto d-block" src="/assets/images/home_dapps.png" alt="Trust Wallet mobile mockup shows list of dapps you can interact within the wallet">
+</div>
+</div>
+</div>
+</section><section class="pt-8 pb-8 pt-sm-10 pb-sm-10">
+<div class="container">
+<div class="row">
+<div class="col-12 text-center mb-2">
+<h3 class="display-4 font-weight-bold">
+Get started in 3 simple steps
+</h3>
+<p class="lead text-gray-700">It only takes a few minutes</p>
+</div>
+</div>
+<div class="row no-gutters mt-8 mb-6 mb-md-7">
+<div class="col-12 col-md-4 text-center">
+<div class="row mb-5">
+<div class="col">
+</div>
+<div class="col-auto icon-xxl">
+<svg alt="icon icon-sm" aria-label="Illustration that shows a man sitting on a bench downloading a wallet">
+<use xlink:href="/assets/images/3steps.svg#download" alt="Illustration that shows a man sitting on a bench downloading a wallet"></use>
+</svg>
+</div>
+<div class="col">
+<hr class="d-none d-md-block mt-8">
+</div>
+</div>
+<h3 class="font-weight-bold">
+Download Trust Wallet
+</h3>
+</div>
+<div class="col-12 col-md-4 text-center">
+<div class="row mb-5">
+<div class="col">
+<hr class="d-none d-md-block mt-8">
+</div>
+<div class="col-auto icon-xxl">
+<svg alt="icon icon-sm" aria-label="Illustration that shows a man creating a crypto wallet while walking">
+<use xlink:href="/assets/images/3steps.svg#wallet" alt="Illustration that shows a man creating a crypto wallet while walking"></use>
+</svg>
+</div>
+<div class="col">
+<hr class="d-none d-md-block mt-8">
+</div>
+</div>
+<h3 class="font-weight-bold">
+Create a new wallet
+</h3>
+</div>
+<div class="col-12 col-md-4 text-center">
+<div class="row mb-5">
+<div class="col">
+<hr class="d-none d-md-block mt-8">
+</div>
+<div class="col-auto icon-xxl">
+<svg alt="icon icon-sm" aria-label="Illustration that shows a man holding phone while buying cryptocurrencies with credit card">
+<use xlink:href="/assets/images/3steps.svg#buy" alt="Illustration that shows a man holding phone while buying cryptocurrencies with credit card"></use>
+</svg>
+</div>
+<div class="col">
+</div>
+</div>
+<h3 class="font-weight-bold">
+Get some crypto
+</h3>
+</div>
+</div>
+<div class="row">
+<div class="col-12 text-center">
+<a href="/download" class="btn btn-primary mx-auto downloadapp-native" aria-label="goes to download page to get Trust Wallet App">
+Download Now
+</a>
+</div>
+</div>
+</div>
+</section>
+</main>
+
+<script>
+    let links = document.querySelectorAll("a[data-analytics]");
+    for (var i = 0; i < links.length; i++) {
+        links[i].addEventListener('click', handleLinkEvent);
+        links[i].addEventListener('auxclick', handleLinkEvent);
+    }
+
+    function handleLinkEvent(event) {
+        var link = event.target;
+        var middle = event.type == "auxclick" && event.which == 2;
+        var click = event.type == "click";
+        while (link && (typeof link.tagName == 'undefined' || link.tagName.toLowerCase() != 'a' || !link.href)) {
+            link = link.parentNode;
+        }
+        if (middle || click) {
+            let attributes = link.getAttribute('data-analytics').split(/,(.+)/);
+            let p = JSON.parse(attributes[1] || '{"props":{}}');
+            p.props["Page url"] = document.location.pathname;
+            let events = [JSON.parse(attributes[0]), p];
+            plausible(...events);
+        }
+        if (!link.target) {
+            if (!(event.ctrlKey || event.metaKey || event.shiftKey) && click) {
+                setTimeout(function () {
+                    location.href = link.href;
+                }, 150);
+                event.preventDefault();
+            }
+        }
+    }
+</script>
+<script>
+    if (window.netlifyIdentity) {
+        window.netlifyIdentity.on("init", user => {
+            if (!user) {
+                window.netlifyIdentity.on("login", () => {
+                    document.location.href = "/admin/";
+                });
+            }
+        });
+    }
+</script>
+</body>
+</html>
